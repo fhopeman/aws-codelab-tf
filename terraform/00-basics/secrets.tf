747c8d0462a6953482409f7fd34e563835b652c0
Fabian Hoffmann <fabian.hoffmann@hanseaticbank.de>
2020-09-14 12:02:03 +0200

    use alias name instead of key id

--- terraform/00-basics/secrets.tf
+++ terraform/00-basics/secrets.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "test" {
   name   = "${var.base_name}-test"
   type   = "SecureString" // String
   value  = "fabian-test-valuer"
-  key_id = aws_kms_alias.ssm.target_key_id
+  key_id = aws_kms_alias.ssm.name
 
   tags = local.default_tags
 }
